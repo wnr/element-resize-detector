2afc2d429754e483e9c4f2dc8049487e0fcc328b
Lucas Wiener <lucas@wiener.se>
2015-03-09 21:22:57 +0100

    Now works in IE8

--- src/browser-detector.js
+++ src/browser-detector.js
@@ -2,7 +2,34 @@
 
 var detector = module.exports = {};
 
-detector.isIE = function() {
-    var agent = navigator.userAgent.toLowerCase();
-    return agent.indexOf("msie") !== -1 || agent.indexOf("trident") !== -1;
+detector.isIE = function(version) {
+    function isAnyIeVersion() {
+        var agent = navigator.userAgent.toLowerCase();
+        return agent.indexOf("msie") !== -1 || agent.indexOf("trident") !== -1;
+    }
+
+    if(!isAnyIeVersion()) {
+        return false;
+    }
+
+    if(!version) {
+        return true;
+    }
+
+    //Shamelessly stolen from https://gist.github.com/padolsey/527683
+    var ieVersion = (function(){
+        var undef,
+            v = 3,
+            div = document.createElement("div"),
+            all = div.getElementsByTagName("i");
+        
+        do {
+            div.innerHTML = "<!--[if gt IE " + (++v) + "]><i></i><![endif]-->";
+        }
+        while (all[0]);
+        
+        return v > 4 ? v : undef;
+    }());
+
+    return version === ieVersion;
 };
--- src/element-utils.js
+++ src/element-utils.js
@@ -22,6 +22,12 @@ utils.getId = function(element) {
  * @returns {boolean} True or false depending on if the element is detectable or not.
  */
 utils.isDetectable = function(element) {
+    if(browserDetector.isIE(8)) {
+        //IE 8 does not use the object method.
+        //Check only if the element has been given an id.
+        return !!utils.getId(element);
+    }
+
     return !!getObject(element);
 };
 
@@ -36,10 +42,17 @@ utils.addListener = function(element, listener) {
         throw new Error("Element is not detectable.");
     }
 
-    var object = getObject(element);
-    object.contentDocument.defaultView.addEventListener("resize", function() {
+    function listenerProxy() {
         listener(element);
-    });
+    }
+
+    if(browserDetector.isIE(8)) {
+        //IE 8 does not support object, but supports the resize event directly on elements.
+        element.attachEvent("onresize", listenerProxy);
+    } else {
+        var object = getObject(element);
+        object.contentDocument.defaultView.addEventListener("resize", listenerProxy);
+    }
 };
 
 /**
@@ -50,54 +63,65 @@ utils.addListener = function(element, listener) {
  * @param {function} callback The callback to be called when the element is ready to be listened for resize changes. Will be called with the element as first parameter.
  */
 utils.makeDetectable = function(element, id, callback) {
-    var OBJECT_STYLE = "display: block; position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: none; padding: 0; margin: 0; opacity: 0; z-index: -1000; pointer-events: none;";
+    function injectObject(element, callback) {
+        var OBJECT_STYLE = "display: block; position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: none; padding: 0; margin: 0; opacity: 0; z-index: -1000; pointer-events: none;";
 
-    function onObjectLoad() {
-        /*jshint validthis:true */
+        function onObjectLoad() {
+            /*jshint validthis:true */
 
-        //Create the style element to be added to the object.
-        var objectDocument = this.contentDocument;
-        var style = objectDocument.createElement("style");
-        style.innerHTML = "html, body { margin: 0; padding: 0 } div { -webkit-transition: opacity 0.01s; -ms-transition: opacity 0.01s; -o-transition: opacity 0.01s; transition: opacity 0.01s; opacity: 0; }";
+            //Create the style element to be added to the object.
+            var objectDocument = this.contentDocument;
+            var style = objectDocument.createElement("style");
+            style.innerHTML = "html, body { margin: 0; padding: 0 } div { -webkit-transition: opacity 0.01s; -ms-transition: opacity 0.01s; -o-transition: opacity 0.01s; transition: opacity 0.01s; opacity: 0; }";
 
-        //TODO: Remove any styles that has been set on the object. Only the style above should be styling the object.
+            //TODO: Remove any styles that has been set on the object. Only the style above should be styling the object.
 
-        //Append the style to the object.
-        objectDocument.head.appendChild(style);
+            //Append the style to the object.
+            objectDocument.head.appendChild(style);
 
-        //TODO: Is this needed here?
-        //this.style.cssText = OBJECT_STYLE;
+            //TODO: Is this needed here?
+            //this.style.cssText = OBJECT_STYLE;
 
-        //Notify that the element is ready to be listened to.
-        callback(element);
+            //Notify that the element is ready to be listened to.
+            callback(element);
+        }
+
+        //The target element needs to be positioned (everything except static) so the absolute positioned object will be positioned relative to the target element.
+        if(getComputedStyle(element).position === "static") {
+            element.style.position = "relative";
+        }
+
+        //Add an object element as a child to the target element that will be listened to for resize events.
+        var object = document.createElement("object");
+        object.type = "text/html";
+        object.style.cssText = OBJECT_STYLE;
+        object.onload = onObjectLoad;
+        object.setAttribute("elq-object-id", id);
+
+        //Safari: This must occur before adding the object to the DOM.
+        //IE: Does not like that this happens before, even if it is also added after.
+        if(!browserDetector.isIE()) {
+            object.data = "about:blank";
+        }
+
+        element.appendChild(object);
+
+        //IE: This must occur after adding the object to the DOM.
+        if(browserDetector.isIE()) {
+            object.data = "about:blank";
+        }
     }
 
     //Create an unique elq-target-id for the target element, so that event listeners can be identified to this element.
     element.setAttribute("elq-target-id", id);
 
-    //The target element needs to be positioned (everything except static) so the absolute positioned object will be positioned relative to the target element.
-    if(getComputedStyle(element).position === "static") {
-        element.style.position = "relative";
-    }
-
-    //Add an object element as a child to the target element that will be listened to for resize events.
-    var object = document.createElement("object");
-    object.type = "text/html";
-    object.style.cssText = OBJECT_STYLE;
-    object.onload = onObjectLoad;
-    object.setAttribute("elq-object-id", id);
-
-    //Safari: This must occur before adding the object to the DOM.
-    //IE: Does not like that this happens before, even if it is also added after.
-    if(!browserDetector.isIE()) {
-        object.data = "about:blank";
-    }
-
-    element.appendChild(object);
-
-    //IE: This must occur after adding the object to the DOM.
-    if(browserDetector.isIE()) {
-        object.data = "about:blank";
+    if(browserDetector.isIE(8)) {
+        //IE 8 does not support objects properly. Luckily they do support the resize event.
+        //So do not inject the object and notify that the element is already ready to be listened to.
+        //The event handler for the resize event is attached in the utils.addListener instead.
+        callback(element);
+    } else {
+        injectObject(element, callback);
     }
 };
 
