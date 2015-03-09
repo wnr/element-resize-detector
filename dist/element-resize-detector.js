c703a054634920b426bd16597f9609021a08c3e9
Lucas Wiener <lucas@wiener.se>
2015-03-09 10:47:52 +0100

    Disted

--- dist/element-resize-detector.js
+++ dist/element-resize-detector.js
@@ -1,10 +1,10 @@
 /*!
- * element-resize-detector 0.1.5 (2015-03-08, 15:10)
+ * element-resize-detector 0.1.5 (2015-03-09, 10:47)
  * https://github.com/wnr/element-resize-detector
  * Licensed under MIT
  */
 
-!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.elementResizeDetectorMaker=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.elementResizeDetectorMaker = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 
 var detector = module.exports = {};
@@ -12,7 +12,8 @@ var detector = module.exports = {};
 detector.isIE = function() {
     var agent = navigator.userAgent.toLowerCase();
     return agent.indexOf("msie") !== -1 || agent.indexOf("trident") !== -1;
-}
+};
+
 },{}],2:[function(require,module,exports){
 "use strict";
 
@@ -56,10 +57,6 @@ module.exports = function(options) {
      * @param {function} listener The callback to be executed for each resize event for each element.
      */
     function listenTo(elements, listener) {
-        function isListenedTo(element) {
-            return elementUtils.isDetectable(element) && eventListenerHandler.get(element).length;
-        }
-
         function onResizeCallback(element) {
             var listeners = eventListenerHandler.get(element);
 
