f8988c46df8a9cd8aec4e589ef17a23faab355c7
Prasanna V. Loganathar <pvl@prasannavl.com>
2016-06-18 07:07:07 +0530

    add margin, padding inline styles for scrollbar size detection

--- src/detection-strategy/scroll.js
+++ src/detection-strategy/scroll.js
@@ -37,10 +37,10 @@ module.exports = function(options) {
         var height = 500;
 
         var child = document.createElement("div");
-        child.style.cssText = "position: absolute; width: " + width*2 + "px; height: " + height*2 + "px; visibility: hidden;";
+        child.style.cssText = "position: absolute; width: " + width*2 + "px; height: " + height*2 + "px; visibility: hidden; margin: 0; padding: 0";
 
         var container = document.createElement("div");
-        container.style.cssText = "position: absolute; width: " + width + "px; height: " + height + "px; overflow: scroll; visibility: none; top: " + -width*3 + "px; left: " + -height*3 + "px; visibility: hidden;";
+        container.style.cssText = "position: absolute; width: " + width + "px; height: " + height + "px; overflow: scroll; visibility: none; top: " + -width*3 + "px; left: " + -height*3 + "px; visibility: hidden; margin: 0; padding: 0";
 
         container.appendChild(child);
 
