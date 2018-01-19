e3447a931f283cd947080a5d3bd2d3b56fbbe60e
Lucas Wiener <lucas@wiener.se>
2018-01-19 09:15:05 +0100

    Disted 1.1.13

--- dist/element-resize-detector.js
+++ dist/element-resize-detector.js
@@ -1,5 +1,5 @@
 /*!
- * element-resize-detector 1.1.12
+ * element-resize-detector 1.1.13
  * Copyright (c) 2016 Lucas Wiener
  * https://github.com/wnr/element-resize-detector
  * Licensed under MIT
@@ -990,7 +990,7 @@ module.exports = function(options) {
                 var width = element.offsetWidth;
                 var height = element.offsetHeight;
 
-                if (width !== element.lastWidth || height !== element.lastHeight) {
+                if (width !== getState(element).lastWidth || height !== getState(element).lastHeight) {
                     debug("Element size changed.");
                     updateDetectorElements(notifyListenersIfNeeded);
                 } else {
